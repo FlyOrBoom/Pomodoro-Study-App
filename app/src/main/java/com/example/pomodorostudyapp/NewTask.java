efd8a27ef422d9ea5c1ae3cba32e6329c0081bf9
Jeffrey Aaron Jeyasingh <jeffreyaaron06@gmail.com>
2020-12-18 16:24:53 -0800

    Added "New Task" Activity
    The New Task Activity is where the user can create a Pomodoro task. It is integrated with the New Task Button on the overview/front page.

--- app/src/main/java/com/example/pomodorostudyapp/NewTask.java
+++ app/src/main/java/com/example/pomodorostudyapp/NewTask.java
@@ -2,13 +2,20 @@ package com.example.pomodorostudyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class NewTask extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("New Task");
+
         setContentView(R.layout.activity_new_task);
     }
+
+
 }
